55434fcba229fce3c0a7669f7aa0acc5b0c1fd30
M. Thoriq Fadlullah <mthoriqfadlullah@outlook.co.id>
2020-06-17 00:27:25 +0800

    edit admin/masjid view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,39 @@ class AdminController extends Controller
 
     public function storemasjid(Request $request)
     {
-        Masjid::create($request->all());
-        return redirect('/admin/masjid') ->with ('status', 'Data masjid berhasil ditambahkan');
+        switch ($request->input('action')) {
+            case 'tambah':
+                Masjid::create($request->all());
+                return redirect('/admin/masjid') ->with ('status', 'Data masjid berhasil ditambahkan');
+                break;
+    
+            case 'kembali':
+                return redirect('/admin/masjid');
+                break;
+        }
+    }
+
+    public function showmasjid(Masjid $masjid)
+    {
+        return view('admin.masjid.edit', compact ('masjid'));
+    }
+
+    public function updatemasjid(Request $request, Masjid $masjid)
+    {
+        switch ($request->input('action')) {
+            case 'edit':
+                Masjid::where('id', $masjid->id)
+                ->update([
+                'nama' => $request->nama,
+                'kontak' => $request->kontak,
+                'alamat' => $request->alamat
+                ]);
+                return redirect('/admin/masjid') ->with ('status', 'Data Masjid Berhasil Diubah!');
+                break;
+    
+            case 'kembali':
+                return redirect('/admin/masjid');
+                break;
+        }
     }
 }
--- resources/views/admin/masjid/edit.blade.php
+++ resources/views/admin/masjid/edit.blade.php
@@ -8,7 +8,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Tambah Data Masjid</h1>
+            <h1 class="m-0 text-dark">Edit Data Masjid</h1>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
@@ -20,23 +20,24 @@
       <div class="container-fluid">
         <div class="row">
             <div class="col">
-                <form method="post" action="/admin/masjid">
+                <form method="post" action="/admin/masjid/{{ $masjid -> id}}">
+                    @method('patch')
                     @csrf
                     <div class="form-group">
                       <label for="nama">Nama Masjid:</label>
-                      <input type="text" class="form-control" id="nama" name="nama">
+                      <input type="text" class="form-control" id="nama" name="nama" value="{{ $masjid -> nama}}">
                     </div>
                     <div class="form-group">
                       <label for="nama">Kontak Masjid:</label>
-                      <input type="text" class="form-control" id="kontak" name="kontak">
+                      <input type="text" class="form-control" id="kontak" name="kontak" value="{{ $masjid -> kontak}}">
                     </div>
                     <div class="form-group">
                       <label for="alamat">Alamat Masjid:</label>
-                      <textarea type="text" class="form-control" id="alamat" name="alamat"></textarea>
+                      <textarea type="text" class="form-control" id="alamat" name="alamat">{{ $masjid -> alamat}}</textarea>
                     </div>
-                    <button type="submit" class="btn btn-primary" name="action" value="tambah">Tambah</button>
+                    <button type="submit" class="btn btn-primary" name="action" value="edit">Edit</button>
                     <button type="submit" class="btn btn-success" name="action" value="kembali">Kembali</button>
-                  </form>
+                </form>
             </div>
         </div>
         <div class="row">
