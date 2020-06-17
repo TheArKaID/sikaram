7bbf4a0202b110416342eefa91b011cece98ad0d
Khairun Nisa <khairun.nisa.ft17@mail.umy.ac.id>
2020-06-17 19:36:44 +0700

    cari masjid

--- resources/views/pages/masjid.blade.php
+++ resources/views/pages/masjid.blade.php
@@ -3,6 +3,19 @@
 @section('content')
 
 <div>ini tempat info masjid</div>
-<div class="row">     <div class="col-md-8">          </div>         <div class="col-md-4">             <form action="/search" method="get">                 <div class="input-group mt-4">                     <input type="search" name="search" class="form-control">                     <span class="input-group-prepend">                         <button type="submit" class="btn btn-success">Search</button>                     </span>                 </div>             </form>         </div>     </div>
+<div class="row">     
+<div class="col-md-8">          
+</div>         
+<div class="col-md-4">             
+<form action="/search" method="get">                 
+<div class="input-group mt-4">                     
+<input type="search" name="search" class="form-control">                     
+<span class="input-group-prepend">                         
+<button type="submit" class="btn btn-success">Search</button>                     
+</span>                 
+</div>             
+</form>         
+</div>     
+</div>
 
 @endsection
