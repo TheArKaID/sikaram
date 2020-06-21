6ab71c2be711395e6243b1ecdcbabef89e9ca175
Arifia Kasastra R <arka.progammer@gmail.com>
2020-06-21 08:50:19 +0700

    Format Document of Front Header and Home

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,5 @@
-
-
-    <header id="header">
-        <nav class="navbar navbar-inverse" role="banner">
+<header id="header">
+    <nav class="navbar navbar-inverse" role="banner">
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -10,16 +8,17 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-<a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
-</div>
-<div class="collapse navbar-collapse navbar-right">
-<ul class="nav navbar-nav">
-<li><a href="/">Home</a></li>
-<li><a href="/masjid">Masjid</a></li>
-<li><a href="/kajian">Jadwal</a></li>
-<li><a href="/mubaligh">Mubaligh</a></li>
-</ul>
-</div>
-</div>
-</nav>
+                <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right">
+                <ul class="nav navbar-nav">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/masjid">Masjid</a></li>
+                    <li><a href="/kajian">Jadwal</a></li>
+                    <li><a href="/mubaligh">Mubaligh</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
